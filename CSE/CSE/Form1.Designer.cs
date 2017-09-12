bec3a2d0f762b22a52ea761725d4f4ae38bc04ee
Lukočius Giedrius <giedrius.lukocius@mif.stud.vu.lt>
2017-09-12 19:07:46 +0300

    Addupload button

--- CSE/CSE/Form1.Designer.cs
+++ CSE/CSE/Form1.Designer.cs
@@ -61,7 +61,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(77, 22);
+            this.textBox2.Location = new System.Drawing.Point(88, 117);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(497, 22);
             this.textBox2.TabIndex = 3;
