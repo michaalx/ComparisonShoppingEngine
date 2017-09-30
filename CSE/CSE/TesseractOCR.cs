76c4eef63bea927f4be52deff519fc5bedeccc72
gretaps <32267310+gretaps@users.noreply.github.com>
2017-10-01 00:04:47 +0300

    Tesseract param change (pageseg).

--- CSE/CSE/TesseractOCR.cs
+++ CSE/CSE/TesseractOCR.cs
@@ -20,8 +20,8 @@ namespace CSE
         {
             var img = new Bitmap(fileName);
             var ocr = new TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndCube);
-            var page = ocr.Process(img);
             ocr.SetVariable("tessedit_pageseg_mode", 6);
+            var page = ocr.Process(img);
             return page.GetText().Replace("\n","\r\n");
         }
     }
