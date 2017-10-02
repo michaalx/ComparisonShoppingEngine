c060c5ede00b6527cadc5782571a3a08c7f453d7
Michal Stankevič <michalito.stankiewicz@gmail.com>
2017-10-02 18:41:04 +0200

    User interface improvement. Lack of back end functionality, will be improved soon.

---
+++ CSE/CSE/FrontEnd/StartForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSE.FrontEnd
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void getDataButton_Click(object sender, EventArgs e)
+        {
+            Hide();
+            GetDataForm getDataForm = new GetDataForm();
+            getDataForm.Show();
+            
+        }
+
+        private void uploadDataButton_Click(object sender, EventArgs e)
+        {
+            Hide();
+            UploadDataForm uploadDataForm = new UploadDataForm();
+            uploadDataForm.Show();
+        }
+    }
+}
