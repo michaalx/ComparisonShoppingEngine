f743211af873fcb8d702d7ca0fab25386e3aa072
Michal Stankevič <michalito.stankiewicz@gmail.com>
2017-10-03 17:28:07 +0200

    Front end improvement. Back end is going to be added soon

---
+++ CSE/CSE/FrontEnd/LogInForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSE.FrontEnd
+{
+    public partial class LogInForm : Form
+    {
+        public LogInForm()
+        {
+            InitializeComponent();
+        }
+        public void emailLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void logInButton_Click(object sender, EventArgs e)
+        {
+            var email = emailBox.Text;
+            var password = passwordBox.Text;
+            var csv = new CSV();
+
+            if (csv.ParsingRegistration(email, password))
+            {
+                this.Hide();
+                Form1 frm = new Form1();
+                frm.Show();
+            }
+            else
+            {
+                errorLabel.Text = "Email or password is incorrect.";
+                //todo: hide after 5 sec .
+            }
+        }
+
+        private void goBackButton_Click(object sender, EventArgs e)
+        {
+            Hide();
+            UploadDataForm uploadDataForm = new UploadDataForm();
+            uploadDataForm.Show();
+        }
+    }
+}
