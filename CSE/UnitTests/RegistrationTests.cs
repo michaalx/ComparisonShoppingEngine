3051d2881ac8edcc1b6c06a027228747fa941b95
Gretaps <greta.pyrantaite@gmail.com>
2017-10-20 11:06:54 +0300

    Product and Registration Unit Tests

--- CSE/UnitTests/RegistrationTests.cs
+++ CSE/UnitTests/RegistrationTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSE;
 
 namespace UnitTests
 {
@@ -7,8 +7,40 @@ namespace UnitTests
     public class RegistrationTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ParsingRegistrationTest1()
         {
+            var user = new User("name", "surname", "email@.com", "password");
+            var csv = new CSV();
+            csv.WriteToFileRegistration(user);
+
+            bool expected = true;
+            bool actual = csv.ParsingRegistration("email@.com", "password");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EmailTest1()
+        {
+            Email userEmail = new Email();
+            string email = "email@gmail.com";
+
+            bool expected = true;
+            bool actual = userEmail.IsValid(email);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EmailTest2()
+        {
+            Email userEmail = new Email();
+            string email = "email.com";
+
+            bool expected = false;
+            bool actual = userEmail.IsValid(email);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
