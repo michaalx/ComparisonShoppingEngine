417435dcd02a482074cbad9b69fa28616cf31433
Michal Stankevič <michalito.stankiewicz@gmail.com>
2017-11-19 06:59:59 +0200

    Add finding the cheapest store by list of products partial implementation. Not fully implemented as not all methods needed to get data from database are created.

---
+++ CSE/Logic/Functions/CheapestStore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Metadata;
+using Logic.Models;
+
+namespace Logic.Functions
+{
+    class CheapestStore : ICheapestStore<string>
+    {
+        public CheapestStore() { }
+
+        public Store GetCheapestStore(IEnumerable<string> products)
+        {
+            var lowestSumOfCart = decimal.MaxValue;
+            var cheapestStore = Store.Maxima;
+            var stores = GetStores();
+            foreach (var store in stores)
+            {
+                var sumOfCart = 0m;
+                foreach(var product in products)
+                {
+                    ///Add price of product in this store to sum of cart.
+                    ///We have to get list of products of store from database.
+                    ///
+                    ///pseudocode of getting price from database 
+                    ///(should be extracted to Reader.cs file or sth like that):
+                    ///SELECT price FROM ProductsTable 
+                    ///WHERE product = productName AND store.ToString() = storeName
+                    ///
+                    ///sumOfCart+= price.Take(1).ToString();
+                    sumOfCart += 0;
+                }
+                if (sumOfCart < lowestSumOfCart)
+                {
+                    cheapestStore = store;
+                    lowestSumOfCart = sumOfCart;
+                }
+            }
+            return cheapestStore;
+        }
+        public IEnumerable<Store> GetStores()
+        {
+            return Enum.GetValues(typeof(Store)).Cast<Store>().ToList();
+        }
+    }
+}
---
+++ CSE/Logic/Functions/ICheapestStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Metadata;
+using Logic.Models;
+
+namespace Logic.Functions
+{
+    interface ICheapestStore<T>
+    {
+        Store GetCheapestStore(IEnumerable<T> products);
+    }
+}
