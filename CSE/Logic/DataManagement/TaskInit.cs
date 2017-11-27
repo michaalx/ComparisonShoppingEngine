cb69cd8b75de017248597d027f1e3be4238d722a
Džiugas Mažulis <dziugas9950@gmail.com>
2017-11-27 03:25:37 +0200

    Update TaskInit.cs

--- CSE/Logic/DataManagement/TaskInit.cs
+++ CSE/Logic/DataManagement/TaskInit.cs
@@ -19,17 +19,14 @@ namespace Logic.DataManagement
 		
 		public void OnListInitialized(object source, EventArgs e)
 		{
-			Thread thread = new Thread(new ThreadStart(UpdateThread));
+			Thread thread = new Thread(new ThreadStart( () =>
+				{
+					DataModel dm = new DataModel(_reader);
+					var updater = new Updater(_configuration);
+					updater.UpdatePopularityRates(Receipt);	
+				}));
 			thread.IsBackground = true;
 			thread.Start();
-		}
-
-		
-		public void UpdateThread()
-		{
-			DataModel dm = new DataModel(_reader);
-			var updater = new Updater(_configuration);
-			updater.UpdatePopularityRates(Receipt);	
 		}
 	}
 }
