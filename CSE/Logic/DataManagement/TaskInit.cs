d7869e5ff168a93980b8b67402c422ddfd6274e4
Džiugas Mažulis <dziugas9950@gmail.com>
2017-11-28 00:14:49 +0200

    Update TaskInit.cs

--- CSE/Logic/DataManagement/TaskInit.cs
+++ CSE/Logic/DataManagement/TaskInit.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Logic.Database;
-using Logic.Metadata;
 using System.Threading;
 using Logic.Models;
-using Microsoft.Extensions.Configuration;
 
 namespace Logic.DataManagement
 {
